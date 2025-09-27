e8bbdddb5758f557be12675371940485b76b3bf3
barankilicci <baran.kilicci@icloud.com>
2025-09-27 15:04:47 +0300

    feat: add neobrutalism theme customization for shadcn/ui and TailwindCSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive">
-        Hey
-      </Button>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant="elevated">
+          I am a button
+        </Button>
+      </div>
+      <div>
+        <Input placeholder="I am an Input"/>
+      </div>
+      <div>
+        <Progress value={40}/>
+      </div>
+      <Textarea placeholder="I am an Textarea"/>
+      <Checkbox />
     </div>
   );
 }
