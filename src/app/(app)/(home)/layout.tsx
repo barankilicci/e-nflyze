fbc31183c4a68d197aca5775cf62b29826b19d53
barankilicci <baran.kilicci@icloud.com>
2025-09-30 23:18:22 +0300

    feat(categories): add join-based subcategories and update layout with formatted data for SearchFilters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,47 @@
+import { Category } from "@/payload-types";
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
+
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 
 interface Props {
   children: React.ReactNode;
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+    depth: 1, // Populate subcategories
+    pagination: false,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      ...(doc as Category),
+      subcategories: undefined
+    })),
+  }));
+
+  console.log({
+    data,
+    formattedData,
+  });
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <SearchFilters data={formattedData} />
       <div className="flex-1">{children}</div>
       <Footer />
     </div>
