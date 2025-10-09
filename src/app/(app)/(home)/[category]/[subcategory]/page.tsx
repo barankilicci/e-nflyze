f5788ee922c84fe3cfd8b1b943e36b3552d02094
barankilicci <baran.kilicci@icloud.com>
2025-10-09 21:36:27 +0300

    feat(categories): implement category & subcategory routing with breadcrumbs and UI enhancements

---
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  params: Promise<{
+    category: string;
+    subcategory: string;
+  }>;
+}
+
+const Page = async ({params}: Props) => {
+  const {category,subcategory } = await params;
+  return <div>Category : {category}
+  Subcategory: {subcategory}</div>;
+};
+
+export default Page;
