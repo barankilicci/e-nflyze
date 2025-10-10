f5a9189a27107412dc5fc1f91e68d465ae96cc68
barankilicci <baran.kilicci@icloud.com>
2025-10-10 16:37:26 +0300

    feat(products): add basic product filtering with NUQS and product-filters component

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,10 +3,11 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "sonner";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,13 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${dmSans.className} antialiased`}
-      >
-        <TRPCReactProvider>
-        {children}
-        <Toaster />
-        </TRPCReactProvider>
+      <body className={`${dmSans.className} antialiased`}>
+        <NuqsAdapter>
+          <TRPCReactProvider>
+            {children}
+            <Toaster />
+          </TRPCReactProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
