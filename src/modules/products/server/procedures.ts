db46dc801bf787cac170170b0d117de968915ad5
barankilicci <baran.kilicci@icloud.com>
2025-10-11 19:24:32 +0300

    feat(tenants): integrate tenant data into product cards and add tenantSlug filtering to getMany procedure

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,4 +1,4 @@
-import { Category, Media } from "@/payload-types";
+import { Category, Media, Tenant } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import type { Sort, Where } from "payload";
 import z from "zod";
@@ -9,29 +9,30 @@ export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
-        cursor:z.number().default(1),
+        cursor: z.number().default(1),
         limit: z.number().default(DEFAULT_LIMIT),
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
         tags: z.array(z.string()).nullable().optional(),
-        sort:z.enum(sortValues).nullable().optional(),
+        sort: z.enum(sortValues).nullable().optional(),
+        tenantSlug: z.string().nullable().optional(),
       })
     )
     .query(async ({ ctx, input }) => {
       const where: Where = {};
       let sort: Sort = "-createdAt";
-      if(input.sort === "curated"){
-        sort = "-createdAt"
-      }  
-
-      if(input.sort === "hot_and_new"){
-        sort = "+createdAt"
+      if (input.sort === "curated") {
+        sort = "-createdAt";
       }
 
-      if(input.sort === "trending"){
-        sort = "-createdAt"
-      }  
+      if (input.sort === "hot_and_new") {
+        sort = "+createdAt";
+      }
+
+      if (input.sort === "trending") {
+        sort = "-createdAt";
+      }
 
       if (input.minPrice && input.maxPrice) {
         where.price = {
@@ -47,6 +48,12 @@ export const productsRouter = createTRPCRouter({
           ...where.price,
           less_than_equal: input.maxPrice,
         };
+      }
+
+      if(input.tenantSlug){
+        where["tenant.slug"] ={
+          equals: input.tenantSlug,
+        }
       }
 
       if (input.category) {
@@ -85,27 +92,28 @@ export const productsRouter = createTRPCRouter({
         }
       }
 
-      if(input.tags && input.tags.length > 0){
-        where["tags.name"] ={
-          in:input.tags,
+      if (input.tags && input.tags.length > 0) {
+        where["tags.name"] = {
+          in: input.tags,
         };
       }
 
       const data = await ctx.db.find({
         collection: "products",
-        depth: 1, // Populate "category" & "image"
+        depth: 2, // Populate "category" & "image", "tenant" & "tenant.image"
         where,
         sort,
         page: input.cursor,
-        limit:input.limit,
+        limit: input.limit,
       });
 
-      return{
+      return {
         ...data,
         docs: data.docs.map((doc) => ({
           ...doc,
           image: doc.image as Media | null,
-        }))
-      }
+          tenant: doc.tenant as Tenant & { image: Media | null },
+        })),
+      };
     }),
 });
